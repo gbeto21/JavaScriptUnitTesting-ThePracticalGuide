2d543f9136aff3d0df8fc0cfafe25504e44548ce
Alberto González <alberto.gonzalez@distillery.com>
2022-07-21 09:38:40 -0500

    The AAA (Arrange, Act, Assert) patter was implemented in the math.test.js file.

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -2,6 +2,16 @@ import { it, expect } from "vitest";
 import { add } from "./math";
 
 it("should sumarize all number values in an array", () => {
-  const result = add([1, 2, 3]);
-  expect(result).toBe(6);
+  //Arrange phase
+  const number = [1, 2, 3];
+  const expectedResult = numbers.reduce(
+    (prevValue, curValue) => prevValue + curValue,
+    0
+  );
+
+  //Act phase
+  const result = add(number);
+
+  //Assert phase
+  expect(result).toBe(expectedResult);
 });
