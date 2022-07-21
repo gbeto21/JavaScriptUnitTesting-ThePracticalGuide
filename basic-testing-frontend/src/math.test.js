85575f54791148393b484d7f215af68cf41dd6af
Alberto González <alberto.gonzalez@distillery.com>
2022-07-21 09:59:02 -0500

    A code refactor was implemented and some negative tests were added.

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -3,15 +3,34 @@ import { add } from "./math";
 
 it("should sumarize all number values in an array", () => {
   //Arrange phase
-  const number = [1, 2, 3];
+  const numbers = [1, 2, 3];
   const expectedResult = numbers.reduce(
     (prevValue, curValue) => prevValue + curValue,
     0
   );
 
   //Act phase
-  const result = add(number);
+  const result = add(numbers);
 
   //Assert phase
+  expect(result).toBe(expectedResult);
+});
+
+it("should yield Nan if a least one invalid number is provided", () => {
+  const inputs = ["invalid", 1];
+
+  const result = add(inputs);
+
+  expect(result).toBeNaN();
+});
+
+it("should yield a correct sum if an array of numeric string values is provided", () => {
+  const numbers = ["1", "2"];
+
+  const result = add(numbers);
+  const expectedResult = numbers.reduce(
+    (prevValue, curValue) => +prevValue + +curValue,
+    0
+  );
   expect(result).toBe(expectedResult);
 });
