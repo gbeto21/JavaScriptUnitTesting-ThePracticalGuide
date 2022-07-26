508f5885c38a607fc31caedf225a82e1c777d3a0
Alberto González <alberto.gonzalez@distillery.com>
2022-07-26 09:37:00 -0500

    A shorter way to test promesies was implemented.

--- async/async/async-example.test.js
+++ async/async/async-example.test.js
@@ -1,16 +1,27 @@
 import { it, expect } from "vitest";
-import { generateToken } from "./async-example";
+import { generateToken, generateTokenPromise } from "./async-example";
 
 it("should generate a token value", (done) => {
   const testUserEmail = "test@test.com";
   generateToken(testUserEmail, (err, token) => {
     // expect(token).toBeDefined();
     try {
-        expect(token).toBeDefined();
-        // expect(token).toBe(2);
-        done();
+      expect(token).toBeDefined();
+      // expect(token).toBe(2);
+      done();
     } catch (error) {
-        done(error);
+      done(error);
     }
   });
+});
+
+it("should generate a token value", () => {
+  const testUserEmail = "test@test.com";
+  expect(generateTokenPromise(testUserEmail)).resolves.toBeDefined();
+});
+
+it("should generate a token value", async () => {
+  const testUserEmail = "test@test.com";
+  const token = await generateTokenPromise(testUserEmail);
+  expect(token).toBeDefined();
 });
